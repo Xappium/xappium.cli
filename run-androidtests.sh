a796a1059f4c63c68069184d68af7d398edbac82
Stephen Hawley <sthawl@microsoft.com>
2021-09-24 11:18:07 -0400

    add in platform names, lowercase the OS.

--- run-androidtests.sh
+++ run-androidtests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dotnet run --project src/Xappium.Cli/Xappium.Cli.csproj -c Release -- test -uitest sample/TestApp.UITests/TestApp.UITests.csproj -app-project sample/TestApp.Android/TestApp.Android.csproj
+dotnet run --project src/Xappium.Cli/Xappium.Cli.csproj -c Release -- android test -uitest sample/TestApp.UITests/TestApp.UITests.csproj -app-project sample/TestApp.Android/TestApp.Android.csproj
